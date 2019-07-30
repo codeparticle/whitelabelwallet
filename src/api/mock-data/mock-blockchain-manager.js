dbb15a992398cfb2429daadbbd2b80983df8ed59
Marc Mathieu <mmathieu.ctr@tenable.com>
2019-07-30 11:12:59 -0400

    feature(wlw-41): setting up mock transactions

--- src/api/mock-data/mock-blockchain-manager.js
+++ src/api/mock-data/mock-blockchain-manager.js
@@ -4,8 +4,14 @@ import { urls } from 'api/mock-data/constants.js';
 const {
   TRANSACTIONS,
   TRANSACTIONS_DETAILS,
+  ADDRESS,
   ADDRESS_DETAILS,
 } = urls;
+
+const charsetParams = {
+  length: 35,
+  charset: '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+};
 
 class mockBlockchainManager {
   /**
@@ -15,8 +21,8 @@ class mockBlockchainManager {
   retrieveTransactionHistory() {
     api.request({
       url: TRANSACTIONS,
-    }).then((response) => response.data)
-      .catch((err) => err);
+    }).then((response) => response.data
+    ).catch((err) => err);
   }
 
   /**
@@ -25,13 +31,10 @@ class mockBlockchainManager {
    */
 
   retrieveTransacationDetails(txid) {
-    console.log(`${TRANSACTIONS_DETAILS}${txid}`);
     api.request({
       url: `${TRANSACTIONS_DETAILS}${txid}`,
-    }).then((response) => {
-      console.log(response.data);
-    })
-      .catch((err) => err);
+    }).then((response) => response.data
+    ).catch((err) => err);
   }
 
   /**
@@ -41,32 +44,69 @@ class mockBlockchainManager {
 
   retrieveAddressDetails(address) {
     api.request({
-      url: `${ADDRESS_DETAILS}=${address}`,
-    }).then((response) => response.data)
-      .catch((err) => err);
+      url: `${ADDRESS_DETAILS}${address}`,
+    }).then((response) => response.data
+    ).catch((err) => err);
   }
 
   /**
    * Retrieve balance of an address
    * @param {string} address
-   * @return {promise} resolves to the balance
+   * @return {promise} resolves to the balance data
    */
   retrieveAddressBalance(address) {
     api.request({
-      url: `${ADDRESS_DETAILS}=${address}`,
+      url: `${ADDRESS_DETAILS}${address}`,
     }).then((response) => {
       const balanceData = {
-        balannce: response.balance,
-        balanceSat: response.balanceSat,
+        balance: response.data[0].balance,
+        balanceSat: response.data[0].balanceSat,
       };
 
-      console.log('========\n', 'response', response, '\n========');
-
-      console.log('========\n', 'balanceData', balanceData, '\n========');
-
       return balanceData;
-    })
-      .catch((err) => err);
+    }).catch((err) => err);
+  }
+
+  /**
+   * Generate random string provided the length of the string
+   * and the character sets to pick chars from
+   * @param {int} length
+   * @param {string} charsets
+   */
+  randomString(length = charsetParams.length, charsets = charsetParams.charset) {
+    let result = '';
+    for (let i = length; i > 0; --i) {
+      result += charsets[Math.round(Math.random() * (charsets.length - 1))];
+    }
+    return result;
+  }
+
+  /**
+   * Generate an address to use in transactions
+   * @param {description} address description
+   * @param {label} address label
+   */
+  generateAddress(description = '', label = '') {
+    const addressData = {
+      address: this.randomString(),
+      isMine: true,
+      isWatchOnly: false,
+      desc: description,
+      isScript: false,
+      isChange: false,
+      isWitness: false,
+      keys: {
+        public: this.randomString(65),
+        private: this.randomString(65),
+      },
+      label,
+      solvable: true,
+    };
+
+    api.post(ADDRESS, addressData).then((response) => {
+      console.log('address generated', response.data);
+      return response.data;
+    }).catch((err) => err);
   }
 }
 
