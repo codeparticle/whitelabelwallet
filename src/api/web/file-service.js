3ddaae9dbd63ea187c4b1e689493a6e77297cdcb
ericky14 <edossantos2014@fau.edu>
2019-07-31 14:44:50 -0400

    WLW-47 - Implemented encryption classes for the db.

---
+++ src/api/web/file-service.js
@@ -0,0 +1,28 @@
+export class FileService {
+  constructor(storage) {
+    this.storage = storage;
+  }
+
+  getKeyName(username, password) {
+    return `whitelabelwallet:${username}:${password}`;
+  }
+
+  // fetch DB file on localStorage given username
+  getDatabaseFile(username, password) {
+    return this.storage.getItem(this.getKeyName(username, password)) || null;
+  }
+
+  // removes DB file on localStorage
+  removeDatabaseFile(username, password) {
+    const keyName = this.getKeyName(username, password);
+
+    if (this.storage.getItem(keyName) !== null) {
+      this.storage.removeItem(keyName);
+    }
+  }
+
+  // stores DB file on localStorage given username
+  storeDatabaseFile(username, password, dbFile) {
+    this.storage.setItem(this.getKeyName(username, password), dbFile);
+  }
+}
