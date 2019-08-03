ef1260918803825f46dabcc31da0ef3b27bcbc41
Marc Mathieu <mmathieu.ctr@tenable.com>
2019-08-02 20:27:35 -0400

    feature(wlw-41): new create transaction functionality

--- src/api/mock-block-chainV2/wallet.js
+++ src/api/mock-block-chainV2/wallet.js
@@ -1,20 +1,26 @@
 import { ec } from 'elliptic';
-import { existsSync, readFileSync,  writeFileSync } from 'fs';
 import * as _ from 'lodash';
+import { api } from 'rdx/api';
 import { Transaction, TxIn, TxOut, TransactionService } from 'api/mock-block-chainV2/transactions';
+import { FileService } from 'api/web/file-service';
+import { urls } from 'api/mock-data/constants';
+
+const {
+  ADDRESS,
+} = urls;
 
 const EC = new ec('secp256k1');
-const privateKeyLocation = 'node/wallet/private_key';
+const fileServiceInst = new FileService(window.localStorage);
 
 class WalletService {
   getPrivateFromWallet() {
-    const buffer = readFileSync(privateKeyLocation, 'utf8');
-    return buffer.toString();
+    return fileServiceInst.getPrivateKey();
   };
 
   getPublicFromWallet() {
     const privateKey = this.getPrivateFromWallet();
     const key = EC.keyFromPrivate(privateKey, 'hex');
+    console.log('========\n', 'public', key.getPublic().encode('hex'), '\n========');
     return key.getPublic().encode('hex');
   };
 
@@ -22,6 +28,33 @@ class WalletService {
     const keyPair = EC.genKeyPair();
     const privateKey = keyPair.getPrivate();
     return privateKey.toString(16);
+  };
+
+  async generateToAddress(description = '', label = '') {
+    const keyPair = EC.genKeyPair();
+    const privateKey = keyPair.getPrivate();
+    const key = EC.keyFromPrivate(privateKey, 'hex');
+    const publicKey = key.getPublic().encode('hex');
+
+    const addressData = {
+      address: publicKey,
+      privateKey,
+      isMine: false,
+      description,
+      label,
+    };
+
+    const response  =  await api.post(ADDRESS, addressData);
+    return response.data;
+  }
+
+  initWallet () {
+    if (fileServiceInst.getPrivateKey() === null) {
+
+      const newPrivateKey = this.generatePrivateKey();
+      fileServiceInst.savePrivateKey(newPrivateKey);
+      console.log('new wallet with private key created');
+    }
   };
 
   /**
@@ -61,14 +94,24 @@ class WalletService {
   };
 
   createTransaction (receiverAddress, amount, privateKey, unspentTxOuts) {
-
+    debugger;
+    // console.log('========\n', '1. receiverAddress', receiverAddress, '\n========');
+    // console.log('========\n', 'amount', amount, '\n========');
+    // console.log('========\n', 'privateKey', privateKey, '\n========');
+    // console.log('========\n', 'unspentTxOuts', unspentTxOuts, '\n========');
     const transactionServiceInst = new TransactionService();
     const myAddress = transactionServiceInst.getPublicKey(privateKey);
     const myUnspentTxOuts = unspentTxOuts.filter((uTxO) => uTxO.address === myAddress);
+    console.log('========\n', 'myUnspentTxOuts', myUnspentTxOuts, '\n========');
 
     const { includedUnspentTxOuts, leftOverAmount } = this.findTxOutsForAmount(amount, myUnspentTxOuts);
 
+    console.log('========\n', 'includedUnspentTxOuts', includedUnspentTxOuts, '\n========');
+    console.log('========\n', 'leftOverAmount', leftOverAmount, '\n========');
+
     const toUnsignedTxIn = (unspentTxOut) => {
+      console.log('========\n', 'mapping over included unspentTxOuts', '\n========');
+      console.log('========\n', 'unspentTxOut', unspentTxOut, '\n========');
       const txIn = new TxIn();
       txIn.txOutId = unspentTxOut.txOutId;
       txIn.txOutIndex = unspentTxOut.txOutIndex;
@@ -80,7 +123,7 @@ class WalletService {
     const tx = new Transaction();
     tx.txIns = unsignedTxIns;
     tx.txOuts = this.createTxOuts(receiverAddress, myAddress, amount, leftOverAmount);
-    tx.id = transactionServiceInst.getTransactionId(tx);
+    tx.txid = transactionServiceInst.getTransactionId(tx);
 
     tx.txIns = tx.txIns.map((txIn, index) => {
       txIn.signature = transactionServiceInst.signTxIn(tx, index, privateKey, unspentTxOuts);
@@ -92,19 +135,6 @@ class WalletService {
 
 }
 
-const initWallet =  () => {
-  const walletServiceInst = new WalletService();
-  // let's not override existing private keys
-  if (existsSync(privateKeyLocation)) {
-    return;
-  }
-  const newPrivateKey = walletServiceInst.generatePrivateKey();
-
-  writeFileSync(privateKeyLocation, newPrivateKey);
-  console.log('new wallet with private key created');
-};
-
 export {
   WalletService,
-  initWallet,
 };
--- src/server/index.js
+++ src/server/index.js
@@ -25,7 +25,7 @@ const buildDirectory = path.resolve(__dirname, '../../build');
 
 app.use(express.static(buildDirectory));
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', `${ORIGIN_URL}:${APP_PORT}`);
+  res.setHeader('Access-Control-Allow-Origin', `${ORIGIN_URL}`);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
