65bf9b4ba1ed9fdfa71c67e063c282ecbbe00ffb
gabewomble <gwomble27@gmail.com>
2019-08-14 13:18:40 -0400

    feature(wlw-97): added media util

---
+++ src/lib/constants/breakpoints.js
@@ -0,0 +1,16 @@
+
+import {
+  widthBreakpointMd,
+  widthBreakpointSm,
+  widthBreakpointXL,
+  widthBreakpointXS,
+  widthBreakpointXXS,
+} from '@codeparticle/whitelabelwallet.styleguide/styles/layout.scss';
+
+export const BREAKPOINTS = {
+  PORTRAIT: widthBreakpointXXS,
+  LANDSCAPE: widthBreakpointXS,
+  TABLET: widthBreakpointSm,
+  DESKTOP: widthBreakpointMd,
+  WIDESCREEN: widthBreakpointXL,
+};
--- src/lib/hooks/index.js
+++ src/lib/hooks/index.js
@@ -1,1 +1,2 @@
 export { useManager } from './use-manager';
+export { useMedia } from './use-media';
