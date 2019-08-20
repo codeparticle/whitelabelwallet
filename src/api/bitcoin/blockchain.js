a32b038f9fea971de01ee2001acbe6ff0d918a46
Marc Mathieu <mmathieu.ctr@tenable.com>
2019-08-20 15:45:33 -0400

    feature(wlw-57): add comments to blockchain and wallets

--- src/api/bitcoin/blockchain.js
+++ src/api/bitcoin/blockchain.js
@@ -24,50 +24,80 @@ class  BitcoinBlockchainManager  extends ApiBlockchainManager {
     this._instance = null;
   }
 
-  // generates a testnet Address
+  /**
+   * This method will generate a testnet address.
+   * @param {string} name.
+   * @return {obj} returns an Address object model.
+   */
   generateAddress = (name = '') => {
     const keyPair = bitcoin.ECPair.makeRandom({ network: walletManager.network });
     const { address } = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey, network: walletManager.network });
     const testnetAddress = new Address(name, address);
-    console.log('========\n', 'address', testnetAddress, '\n========');
     return testnetAddress;
   }
 
-  // gets details for a specific address
+  /**
+   * This method gets details for a specific address.
+   * @param {string} name.
+   * @return {obj} returns an Address object model.
+   */
   getAddressDetails = async (addressParam) => {
     const rawAddress = (await api.get(`${ADDRESS}/${addressParam}`)).data.address;
     const address = new Address('', rawAddress);
     return address;
   }
 
-  // gets recent unconfirmed transactions;
+  /**
+   * This method gets recent unconfirmed transactions.
+   * @return {obj} returns an Transaction object model.
+   */
   getTransactions = async () => {
     const rawTxsData = (await api.get(TRANSACTIONS)).data;
     const transactions = rawTxsData.map(this.bitcoinTransactionFormatter);
     return transactions;
   }
 
-  // get details of specific transaction
+  /**
+   * This method get details of specific transaction. It takes transaction hash as a param
+   * @param {string} txid.
+   * @return {obj} returns an Transaction object model.
+   */
   getTransactionDetails = async (txid) => {
     const rawTxData = (await  api.get(`${TRANSACTIONS}/${txid}`)).data;
     return this.bitcoinTransactionFormatter(rawTxData);
   }
 
-  // get balance for an address
+  /**
+   * This method gets the balance for an address
+   * @return {number} returns number value.
+   */
   getBalanceForAddress = async (addressParam) => {
     const balanceData = (await api.get(`${ADDRESS}/${addressParam}`)).data.balance;
     return balanceData;
   }
 
-  // sends 49000 satoshis to a randomly generated testnet address
+  /**
+   * This method sends a fix value of 49000 satoshis to a randomly generated testnet address.
+   * Some values are hard coded for testing purposes
+   * @param {string} previousHash
+   * @param {number} uTxOIndex
+   * @return {number} returns number value.
+   */
   sendToAddress = async (previousHash, uTxOIndex) => {
+    // generate a receiver testnet address
     const receiverTestKeyPair = bitcoin.ECPair.makeRandom({ network: walletManager.network });
     const { address: receiverAddress } = bitcoin.payments.p2pkh({ pubkey: receiverTestKeyPair.publicKey, network: walletManager.network });
+
+    // get your wallet's key pair from your private key.
     const myKeyPair = bitcoin.ECPair.fromWIF(walletManager.pk, walletManager.network);
+
+    // set up the transactions variables: the fees, amount to keep, and amount to send;
     const totalFunds = await this.getBalanceForAddress(walletManager.addr);
     const fundsToKeep = totalFunds - 50000;
     const transactionFee = 1000;
     const amountToSend = totalFunds - fundsToKeep - transactionFee;
+
+    // create a new Transaction using bitcoin-lib.js transaction builder.
     const tx = new bitcoin.TransactionBuilder(walletManager.network);
     tx.addInput(previousHash, uTxOIndex);
     tx.addOutput(receiverAddress, amountToSend);
@@ -75,15 +105,17 @@ class  BitcoinBlockchainManager  extends ApiBlockchainManager {
     tx.sign(0, myKeyPair);
     const rawTxHex = tx.build().toHex();
     const newTxData = (await api.post(BROADCAST_TRANSACTION, { 'tx': rawTxHex })).data.tx;
+
+    // Format the newly created transaction;
     const transaction = this.bitcoinTransactionFormatter(newTxData);
-    console.log('========\n', 'transaction', transaction, '\n========');
     return transaction;
   }
 
-  rng = () => {
-    return Buffer.from('YT8dAtK4d16A3P1z+TpwB2jJ4aFH3g9M1EioIBkLEV4=', 'base64');
-  }
-
+  /**
+   * This function takes raw transactions and converts them to our desired Transaction model.
+   * @param {obj} rawTx
+   * @return {obj} returns a obj formatted to our Transaction Model.
+   */
   bitcoinTransactionFormatter = (rawTx) => {
     const senderAddresses = this.addressAggregator(rawTx.inputs);
     const recipientAddresses = this.addressAggregator(rawTx.outputs);
@@ -99,10 +131,18 @@ class  BitcoinBlockchainManager  extends ApiBlockchainManager {
     return tx;
   }
 
+  /**
+   * Helper function to get the wallets public address
+   */
   getMyAddress = () => {
     return walletManager.addr;
   }
 
+  /**
+   * This function is used to parse txIns and txOuts and retrieve all the addresses
+   * used in the transaction inputs and outputs.
+   * @param {obj} txData
+   */
   addressAggregator(txData) {
     const aggregateData = txData.map((group) => {
       if (group.addresses !== undefined && group.addresses !== null) {
--- src/api/bitcoin/wallet.js
+++ src/api/bitcoin/wallet.js
@@ -3,6 +3,10 @@ import { FileManager } from 'api/web/file-manager';
 
 const fileManager = new FileManager(window.localStorage);
 const testNetwork = bitcoin.networks.testnet;
+/**
+ * This function can be used to generate and new private/public key pair used by the wallet.
+ * These values are saved to local storage.
+ */
 const init = () => {
   if (fileManager.getPrivateKey() === null) {
     const keyPair = bitcoin.ECPair.makeRandom({ network: testNetwork });
