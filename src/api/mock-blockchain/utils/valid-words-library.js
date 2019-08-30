4c79273c728dc6ab8e31e7ba13772ae58661b073
Marc Mathieu <mmathieu.ctr@tenable.com>
2019-08-30 14:46:57 -0400

    feature(wlw-73): set up bip39 word array

---
+++ src/api/mock-blockchain/utils/valid-words-library.js
@@ -0,0 +1,22 @@
+import { wordlists as englishWordlists } from 'bip39';
+
+/**
+ * wordlists library
+ * @param {string} EN
+ * @param {string} JA
+ * @param {string} chinese_simplified
+ * @param {string} chinese_traditional
+ * @param {string} english
+ * @param {string} french
+ * @param {string} italian
+ * @param {string} japanese
+ * @param {string} korean
+ * @param {string} spanish
+ */
+
+const wordlists = englishWordlists;
+
+// add additional language here:
+// example: wordlists.german = [word list array];
+
+export default wordlists;
