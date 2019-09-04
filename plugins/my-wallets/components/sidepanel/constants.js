d9aec7e0f791a24a5067b905a6a1f0d151f80bac
Marc Mathieu <mmathieu.ctr@tenable.com>
2019-09-04 10:59:44 -0400

    featue(wlw-124): set up step 2 of wallet creation flow

--- plugins/my-wallets/components/sidepanel/constants.js
+++
@@ -1,9 +0,0 @@
-const DEFAULT_WORDS = [
-  'thank', 'wagon', 'verb', 'tree', 'pear', 'apple', 'jealous', 'phone',
-  'air', 'quiet', 'noun', 'flower', 'cement', 'door', 'lunar', 'always',
-  'one', 'bark', 'shirt', 'closed', 'ship', 'plane', 'test', 'quiz',
-];
-
-export {
-  DEFAULT_WORDS,
-};
