005314c82963c3f1a3c92d7d3185b49bb6542aab
Marc Mathieu <mmathieu.ctr@tenable.com>
2019-09-05 18:13:12 -0400

    feature(wlw-75): set up db insert and redux logic for wallet creation

--- plugins/my-wallets/pages/my-wallets/my-wallets.jsx
+++ plugins/my-wallets/pages/my-wallets/my-wallets.jsx
@@ -6,8 +6,8 @@ import { HeaderButton, IconButton, svgs } from '@codeparticle/whitelabelwallet.s
 import { Visible } from '@codeparticle/react-visible';
 import { Page } from 'components';
 import { empty } from 'lib/utils';
-
-import { clearWalletData } from 'plugins/my-wallets/rdx/actions';
+import { useManager } from 'lib/hooks';
+import { setWallets } from 'plugins/my-wallets/rdx/actions';
 import { MY_WALLETS } from 'plugins/my-wallets/translations/keys';
 import { WalletSidepanel, Wallets } from 'plugins/my-wallets/components';
 
@@ -29,13 +29,11 @@ const MyWallets = ({
   intl: {
     formatMessage,
   },
-  clearWalletData,
 }) => {
   // Load wallets from local DB
   const wallets = [];
   const onClose = ()=> {
     setIsOpenValue(false);
-    clearWalletData();
   };
   const [isOpenValue, setIsOpenValue] = useState(false);
   const AddWallet = () => (
@@ -45,6 +43,7 @@ const MyWallets = ({
       onClick={() => setIsOpenValue(true)}
     />
   );
+  const manager = useManager();
 
   return (
     <Page
@@ -58,18 +57,21 @@ const MyWallets = ({
       <WalletSidepanel
         onClose={onClose}
         intl
-        isOpen={isOpenValue} />
+        isOpen={isOpenValue}
+        manager={manager}
+        setIsOpenValue={setIsOpenValue}
+        setWallets={setWallets}/>
     </Page>
   );
 };
 
 MyWallets.propTypes = {
   intl: intlShape.isRequired,
-  clearWalletData: PropTypes.func.isRequired,
+  setWallets: PropTypes.func.isRequired,
 };
 
 const mapDispatchToProps = {
-  clearWalletData,
+  setWallets,
 };
 
 const MyWalletsPage = connect(null, mapDispatchToProps)(injectIntl(MyWallets));
