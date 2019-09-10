d92656bdd3f9b7acfe7dc4de2f0530ecd5a4f382
gabewomble <gwomble27@gmail.com>
2019-09-10 15:20:20 -0400

    feature(wlw-119): manage wallet panel pt 1

--- plugins/my-wallets/helpers/queries.js
+++ plugins/my-wallets/helpers/queries.js
@@ -16,11 +16,22 @@ async function fetchWallets(manager, setFn) {
 }
 
 /**
-   * Function that adds a new wallet to the db, and then sets the updated wallet table to state
-   * @param {Object} manager - the manager object
-   * @param {func} setFn - function that sets the response to state
-   * @param {Object} wallet - the wallet object to add to the db
-   */
+ * Function to get a single wallet from db by ID
+ * @param {Object} manager - the manager object
+ * @param {func} setFn - function that sets the res to state
+ * @param {number} id - id of wallet to get
+ */
+async function getWalletById(manager, setFn, id) {
+  const res = await manager.databaseManager.getWalletById(id);
+  setFn(res);
+}
+
+/**
+ * Function that adds a new wallet to the db, and then sets the updated wallet table to state
+ * @param {Object} manager - the manager object
+ * @param {func} setFn - function that sets the response to state
+ * @param {Object} wallet - the wallet object to add to the db
+ */
 async function createWalletAndUpdateList(manager, setFn, wallet) {
   await manager.databaseManager.insert().wallet(wallet);
   await manager.saveDatabase();
@@ -28,8 +39,24 @@ async function createWalletAndUpdateList(manager, setFn, wallet) {
   await fetchWallets(manager, setFn);
 }
 
+/**
+ * Function that updates a wallet by id, and then updates the selected
+ * wallet in state.
+ * @param {Object} manager - the manager object
+ * @param {func} setFn - function that sets the response to state
+ * @param {Object} wallet - the wallet object to add to the db
+ */
+async function updateWalletAndUpdateState(manager, setFn, wallet) {
+  await manager.databaseManager.updateWalletById(wallet.id, wallet);
+  await manager.saveDatabase();
+
+  await getWalletById(manager, setFn, wallet.id);
+}
+
 
 export {
   createWalletAndUpdateList,
   fetchWallets,
+  getWalletById,
+  updateWalletAndUpdateState,
 };
