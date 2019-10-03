f9553f841589cf04e4eac4c1ed5ae9cd6ff61290
Marc Mathieu <mmathieu.ctr@tenable.com>
2019-10-03 12:07:49 -0400

    feature(wlw-111): change custom render names and document build chart data function

--- src/api/db/statements/transactions/index.js
+++ src/api/db/statements/transactions/index.js
@@ -10,7 +10,7 @@ export const TRANSACTIONS_STATEMENTS = {
     sender_address varchar(100),
     receiver_address varchar(100),
     status tinyint not null,
-    created_date dateTime,
+    created_date datetime,
     transaction_type varchar(100),
     pending_balance decimal(16,8),
     foreign key (sender_address_id) references Addresses(id) on delete set null,
@@ -21,8 +21,8 @@ export const TRANSACTIONS_STATEMENTS = {
     values(?,?,?,?,?,?,?,?,?,?,?,?,?)`,
   },
   SELECT: {
-    ALL: `select * from Transactions`,
-    PER_ADDRESS: (address, dateTime) => `select * from Transactions where (sender_address = "${address}" or receiver_address = "${address}") and created_date >= "${dateTime}"`,
-    VALUE: (address, value, dateTime) => `select Transactions.*, Addresses.name from Transactions left join Addresses on (receiver_address = Addresses.address or sender_address = Addresses.address) where (description like "%${value}%" or amount like "%${value}%" or Transactions.created_date like "%${value}%" or name like "%${value}%") and (sender_address = "${address}" or receiver_address = "${address}") and Transactions.created_date >= "${dateTime}"`,
+    ALL: `select * from Transactions order by created_date desc`,
+    PER_ADDRESS: (address, dateTime) => `select * from Transactions where (sender_address = "${address}" or receiver_address = "${address}") and created_date >= "${dateTime}" order by created_date desc`,
+    VALUE: (address, value, dateTime) => `select Transactions.*, Addresses.name from Transactions left join Addresses on (receiver_address = Addresses.address or sender_address = Addresses.address) where (description like "%${value}%" or amount like "%${value}%" or Transactions.created_date like "%${value}%" or name like "%${value}%") and (sender_address = "${address}" or receiver_address = "${address}") and Transactions.created_date >= "${dateTime}" order by created_date desc`,
   },
 };
