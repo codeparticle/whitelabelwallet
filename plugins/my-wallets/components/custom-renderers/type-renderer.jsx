5931d29338c7684d695ad50506c0fba7a069bd6b
Marc Mathieu <mmathieu.ctr@tenable.com>
2019-10-10 20:05:38 -0400

    feature(wlw-86): address pr comments

--- plugins/my-wallets/components/custom-renderers/type-renderer.jsx
+++ plugins/my-wallets/components/custom-renderers/type-renderer.jsx
@@ -3,15 +3,15 @@ import PropTypes from 'prop-types';
 import {
   svgs,
 } from '@codeparticle/whitelabelwallet.styleguide';
-import { TYPES } from 'plugins/my-wallets/helpers';
+import { TRANSACTION_TYPES } from 'plugins/my-wallets/helpers';
 
 
 const  { SvgReceive, SvgSend } = svgs.icons;
-const { RECEIVE } = TYPES;
+const { RECEIVE } = TRANSACTION_TYPES;
 
 function CustomTypeRenderer ({ data }) {
   const Icon = data.transaction_type === RECEIVE
-    ?  SvgReceive
+    ? SvgReceive
     : SvgSend;
   return (
     <Icon className="transaction-type" />
