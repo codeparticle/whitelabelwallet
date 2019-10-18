5272793cf02e57baa572a929b164aba94e24b05d
Marc Mathieu <mmathieu.ctr@tenable.com>
2019-10-18 16:45:02 -0400

    feature(wlw-101): refactor useEffects, organize css, and deconstruct props

--- plugins/transaction-history/components/transaction-chart/transaction-chart.jsx
+++ plugins/transaction-history/components/transaction-chart/transaction-chart.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import {
   AreaChart,
@@ -12,49 +12,49 @@ function TransactionChart ({
   transactions,
   minimumNumberOfChartPoints,
 }) {
-
   const [chartDataPoints, setChartDataPoints] = useState([]);
+
+  const buildChartData = useCallback(
+    async () => {
+      if (transactions.length === 0) {
+        return [];
+      }
+
+      const chronologicalTransactions = transactions.reverse();
+
+      // Current balance is equal to the balance of the latest transaction.
+      const currentBalance = chronologicalTransactions.length > 0
+        ? chronologicalTransactions[chronologicalTransactions.length - 1].pending_balance
+        : 0;
+
+      // Create array of point coordinates using the chronologicalTransactions
+      const chartData = chronologicalTransactions.map((transaction, index) => {
+        return { x: index + 1, y: transaction.pending_balance };
+      });
+
+      // Check if we have enough transactions to build the chart, if so, set the chartData in state.
+      if (chartData.length >= minimumNumberOfChartPoints) {
+        setChartDataPoints(chartData);
+        return;
+      }
+
+      // If not, calculate the number of remaining points to plot on the chart.
+      const numberOfRemainingChartPoints = minimumNumberOfChartPoints - chronologicalTransactions.length;
+
+      // Since there are not enough transactions over the last three months then we create point coordinates using the balance of the most recent transaction available.
+      for (let counter = minimumNumberOfChartPoints - numberOfRemainingChartPoints; counter < minimumNumberOfChartPoints; counter++) {
+        chartData.push({ x: counter, y: currentBalance });
+      }
+
+      setChartDataPoints(chartData);
+    }, [transactions],
+  );
 
   useEffect(() => {
     if (transactions.length > 0) {
       buildChartData();
     }
-  }, [transactions]);
-
-  const buildChartData = async () => {
-    if (transactions.length === 0) {
-      return [];
-    }
-
-
-    const chronologicalTransactions = transactions.reverse();
-
-    // Current balance is equal to the balance of the latest transaction.
-    const currentBalance = chronologicalTransactions.length > 0
-      ? chronologicalTransactions[chronologicalTransactions.length - 1].pending_balance
-      : 0;
-
-    // Create array of point coordinates using the chronologicalTransactions
-    const chartData = chronologicalTransactions.map((transaction, index) => {
-      return { x: index + 1, y: transaction.pending_balance };
-    });
-
-    // Check if we have enough transactions to build the chart, if so, set the chartData in state.
-    if (chartData.length >= minimumNumberOfChartPoints) {
-      setChartDataPoints(chartData);
-      return;
-    }
-
-    // If not, calculate the number of remaining points to plot on the chart.
-    const numberOfRemainingChartPoints = minimumNumberOfChartPoints - chronologicalTransactions.length;
-
-    // Since there are not enough transactions over the last three months then we create point coordinates using the balance of the most recent transaction available.
-    for (let counter = minimumNumberOfChartPoints - numberOfRemainingChartPoints; counter < minimumNumberOfChartPoints; counter++) {
-      chartData.push({ x: counter, y: currentBalance });
-    }
-
-    setChartDataPoints(chartData);
-  };
+  }, [transactions, buildChartData]);
 
   return (
     <AreaChart
