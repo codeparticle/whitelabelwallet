94c6ef94eb9f3b9f093ff636ac48354525bac002
Marc Mathieu <mmathieu.ctr@tenable.com>
2019-10-22 12:26:52 -0400

    feature(wlw-101): refactor sort func and remove contants

--- src/lib/utils/sort-by-date-desc.js
+++ src/lib/utils/sort-by-date-desc.js
@@ -1,8 +1,7 @@
 function sortListByDate (data) {
   const sortedTransactions = data.sort(function(date1, date2) {
     if (date1.created_date > date2.created_date) {
-      return -1
-      ;
+      return -1;
     };
     if (date1.created_date < date2.created_date) {
       return 1;
