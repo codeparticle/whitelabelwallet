17ae09ef1ebee426fda9c8b96ee0cdcad9274ee8
Marc Mathieu <mmathieu.ctr@tenable.com>
2019-10-24 10:56:45 -0400

    feature(wlw-104): set up moble transaction detail complete

--- plugins/transaction-history/components/transaction-details-sidepanel/transaction-details-sidepanel.jsx
+++ plugins/transaction-history/components/transaction-details-sidepanel/transaction-details-sidepanel.jsx
@@ -8,29 +8,18 @@ import {
   useMedia,
 } from '@codeparticle/whitelabelwallet.styleguide';
 import { getSidepanelVariant } from 'lib/utils';
-import { CustomAmountRenderer, CustomWalletRenderer, TransactionDetailContent, TransactionRow } from 'plugins/transaction-history/components';
+import { TransactionDetailContent } from 'plugins/transaction-history/components';
 import { TRANSACTION_HISTORY } from 'plugins/transaction-history/translations/keys';
-import { TRANSLATION_KEYS } from 'translations/keys';
-import { COMPLETE_STATUS } from 'plugins/transaction-history/helpers';
 import './transaction-details-sidepanel.scss';
 
-const { COMMON } = TRANSLATION_KEYS;
 const { SvgSend } = svgs.icons;
 const {
-  COMPLETE_LABEL,
   TRANSACTION_DETAILS_TITLE,
   TRANSACTION_DETAILS_BUTTON,
 } = TRANSACTION_HISTORY;
 
 const {
-  AMOUNT,
   DATE_TIME,
-  DESCRIPTION,
-  PENDING,
-  STATUS,
-  SENT_FROM,
-  SENT_TO,
-  WALLET,
 } = COMMON;
 
 function TransactionDetailsSidepanel({
@@ -46,35 +35,21 @@ function TransactionDetailsSidepanel({
     time: moment(selectedTransaction.created_date).format('h:mm a'),
   });
 
-  function getStatus(status = null) {
-    if (status === COMPLETE_STATUS) {
-      return (
-        <div className="complete">
-          {formatMessage(COMPLETE_LABEL)}
-        </div>
-      );
-    }
-
-    return (
-      <div className="pending">
-        {formatMessage(PENDING)}
-      </div>
-    );
-  }
-
-
   function getContent() {
     if (isMobile) {
       return (
         <ReceiptContainer>
-          <TransactionDetailContent formatMessage={formatMessage} selectedTransaction={selectedTransaction} />
+          <TransactionDetailContent
+            formattedDate={formattedDate}
+            formatMessage={formatMessage}
+            selectedTransaction={selectedTransaction}
+          />
         </ReceiptContainer>
       );
     }
 
     return <TransactionDetailContent formatMessage={formatMessage} selectedTransaction={selectedTransaction} />;
   }
-
 
 
   return (
@@ -89,19 +64,6 @@ function TransactionDetailsSidepanel({
       subTitle={formattedDate}
       type={panelVariant}
     >
-      {/* <div className="transaction-detail-content">
-        <div className="row">
-          <div className="field">
-            <label>{formatMessage(WALLET_LABEL)}</label>
-            <CustomWalletRenderer data={selectedTransaction}/>
-          </TransactionRow>
-          <TransactionRow label={formatMessage(AMOUNT)}>
-            <div className="amount-wrapper">
-              <CustomAmountRenderer data={selectedTransaction} column={selectedTransaction.amount}/>
-            </div>
-          </TransactionRow>
-        </div>
-      </div> */}
       {getContent()}
     </Overlay>
   );
