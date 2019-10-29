d08531ad70a06864fb4c9f4139fd7bcb1cc358a0
Marc Mathieu <mmathieu.ctr@tenable.com>
2019-10-29 08:57:35 -0400

    featue(wlw-173): refactor build wallet chart function

--- plugins/my-wallets/components/wallets/wallets.jsx
+++ plugins/my-wallets/components/wallets/wallets.jsx
@@ -94,13 +94,15 @@ const WalletsView = ({
         return chartData;
       }
 
-      // If not, calculate the number of remaining points to plot on the chart.
-      const numberOfRemainingChartPoints = MINIMUM_NUMBER_CHART_POINTS - distinctTransactions.length;
+      if (chartData.length < MINIMUM_NUMBER_CHART_POINTS) {
+        // If not, calculate the number of remaining points to plot on the chart.
+        const numberOfRemainingChartPoints = MINIMUM_NUMBER_CHART_POINTS - distinctTransactions.length;
 
-      // Since there are not enough transactions over the last two months then we create point coordinates using the balance of the most recent transaction available.
-      for (let counter = MINIMUM_NUMBER_CHART_POINTS - numberOfRemainingChartPoints; counter < MINIMUM_NUMBER_CHART_POINTS; counter++) {
-        chartData.push({ x: counter, y: currentBalance });
-      }
+        // Since there are not enough transactions over the last two months then we create point coordinates using the balance of the most recent transaction available.
+        for (let counter = MINIMUM_NUMBER_CHART_POINTS - numberOfRemainingChartPoints; counter < MINIMUM_NUMBER_CHART_POINTS; counter++) {
+          chartData.push({ x: counter, y: currentBalance });
+        }
+      };
 
       return chartData;
     }, [],
