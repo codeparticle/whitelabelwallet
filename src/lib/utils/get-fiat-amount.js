7a4fc561070180b8bf18e375be9f92ed3e46825d
Marc Mathieu <mmathieu.ctr@tenable.com>
2019-11-20 12:16:37 -0500

    feature(wlw-180): remove api-key

--- src/lib/utils/get-fiat-amount.js
+++ src/lib/utils/get-fiat-amount.js
@@ -3,12 +3,20 @@ import axios from 'axios';
 import { environment } from './environment';
 
 const { coinApiKey, coin = 'btc', fiat = 'usd' } =  environment;
+
 const URL = `https://rest.coinapi.io/v1/exchangerate/${coin.toUpperCase()}/${fiat.toUpperCase()}`;
 const config = {
   headers: { 'X-CoinAPI-Key': coinApiKey },
 };
 
 const getFiatAmount = async (amountToConvert = 0) => {
+  if (coinApiKey === null) {
+    return {
+      amount: 0,
+      rate: 0,
+    };
+  }
+
   const { rate = 0 } = (await axios.get(URL, config)).data;
   return {
     amount: (amountToConvert * rate).toFixed(2),
